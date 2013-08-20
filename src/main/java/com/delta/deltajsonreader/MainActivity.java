05c4ec7d96192f8416a5caba316a36a0cbfcfbac
kinfongiscool <bradley.kin.fong@gmail.com>
2013-08-19 21:23:29 -0500

    working build: prints all 4 RT lines

--- src/main/java/com/delta/deltajsonreader/MainActivity.java
+++ src/main/java/com/delta/deltajsonreader/MainActivity.java
@@ -18,7 +18,10 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainActivity extends Activity {
 
@@ -30,7 +33,34 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         String s = getJSONFile();
-        String myDataArray[] = {};
+        ArrayList<String> myDataArray = new ArrayList<String>();
+
+        try {
+//            JSONObject jsonObject = new JSONObject(s);
+//
+//            Set<String> keySet = jsonObject.keySet();
+//
+//            while(keys.hasNext()) {
+//                String key = (String)keys.next();
+//                if(key.contains("Text")) {
+//                    String tempString = key.();
+//                    if(tempString.contains("RT ")) {
+//                        mOutput.setText()
+//                    }
+//                }
+//            }
+
+            JSONArray jsonArray = new JSONArray(s);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if ((jsonObject.getString("text")).contains("RT ")) {
+                    myDataArray.add(jsonObject.getString("text"));
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
 
         //setup my listView
@@ -53,7 +83,7 @@ public class MainActivity extends Activity {
         String json = null;
         try {
 
-            InputStream is = getResources().openRawResource(R.raw.samplejson);
+            InputStream is = getResources().openRawResource(R.raw.samplejsontwitter);
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
